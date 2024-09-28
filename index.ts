ed776d91a15f9b9a4f37f85a5a4f453ce01d95cf
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-28 22:18:18 +0700

    Tạo router cơ bản

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Trang chu")
+app.get("/tours", (req: Request, res: Response) => {
+    res.send("Danh sach tours")
 });
 
 app.listen(port, () => {
