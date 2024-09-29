adc9003983729a1abfca13e1a72b6bccb4829ee6
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-29 16:48:21 +0700

    Cấu trúc lại folder Pug

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,15 +1,20 @@
-import { Router, Request, Response } from "express";
+import { Request, Response } from "express";
 
 import Tour from "../../models/tour.model";
 
 //[GET] /tours
 export const index = async (req: Request, res: Response) => {
   const tours = await Tour.findAll({
+    where: {
+        deleted: false,
+        status: "active"
+    },
     raw: true,
   });
 
   console.log(tours);
   res.render("client/pages/tours/index.pug", {
+    pageTitle: "Danh sách tour",
     tours: tours,
   });
 };
