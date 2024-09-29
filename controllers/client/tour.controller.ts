26c45f8d0f0b691a7a0c258446610ef1256fe82e
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-29 22:14:45 +0700

    Làm trang danh sách tour theo danh mục (client)

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,18 +1,56 @@
 import { Request, Response } from "express";
 
 import Tour from "../../models/tour.model";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 
-//[GET] /tours
+//[GET] /tours/:slugCategory
 export const index = async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    where: {
-        deleted: false,
-        status: "active"
-    },
-    raw: true,
+  const slugCategory = req.params.slugCategory;
+
+  /*
+  SELECT tours.*, price * (1 - discount/100) AS price_special
+  FROM tours
+  JOIN tours_categories ON tours.id = tours_categories.tour_id
+  JOIN categories ON tours_categories.category_id = categories.id
+  WHERE
+  categories.slug   
+  = 'du-lich-trong-nuoc'
+  AND categories.deleted = false
+  AND categories.status = 'active'
+  AND tours.deleted = false
+  AND tours.status = 'active';
+  */
+
+  const tours = await sequelize.query(
+    `
+      SELECT tours.*, ROUND(price * (1 - discount/100), 0) AS price_special
+      FROM tours
+      JOIN tours_categories ON tours.id = tours_categories.tour_id
+      JOIN categories ON tours_categories.category_id = categories.id
+      WHERE
+      categories.slug = '${slugCategory}'
+      AND categories.deleted = false
+      AND categories.status = 'active'
+      AND tours.deleted = false
+      AND tours.status = 'active';
+    `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  tours.forEach((item) => {
+    if ((item as any).images) {
+      const images = JSON.parse((item as any).images);
+      (item as any).image = images[0];
+    }
+
+    (item as any).price_special = parseFloat((item as any).price_special);
   });
 
   console.log(tours);
+
   res.render("client/pages/tours/index.pug", {
     pageTitle: "Danh sách tour",
     tours: tours,
--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -3,6 +3,6 @@ const router: Router = Router();
 
 import * as controller from "../../controllers/client/tour.controller";
 
-router.get("/", controller.index);
+router.get("/:slugCategory", controller.index);
 
 export const tourRoutes: Router = router;
