3bbb9a6760204c671c8c9116f0c3e028e8de892a
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-30 16:40:53 +0700

    Hiển thị ảnh dạng slider

---
+++ public/client/js/script.js
@@ -0,0 +1,18 @@
+// Slider Tour Detail
+var imagesThumb = new Swiper(".imagesThumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var imagesMain = new Swiper(".imagesMain", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: imagesThumb,
+  },
+});
+// End Slider Tour Detail
