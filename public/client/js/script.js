907d12d5743d5db3d3be0ae03cd15bde0ebbe5e6
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-30 21:24:41 +0700

    Thêm tour vào giỏ hàng

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -16,3 +16,42 @@ var imagesMain = new Swiper(".imagesMain", {
   },
 });
 // End Slider Tour Detail
+
+// Carts
+
+// Nếu chưa có giỏ hàng trong localStorage thì tạo giỏ hàng mới cho người dùng
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Thêm tour vào giỏ hàng
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExitsTour = cart.findIndex((item) => item.tourId == tourId);
+
+      if (indexExitsTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity,
+        });
+      } else {
+        cart[indexExitsTour].quantity =
+          cart[indexExitsTour].quantity + quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// End Carts
