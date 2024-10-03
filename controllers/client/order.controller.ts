4c9572106b410288bacb9cd643279184491886d7
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-10-03 10:20:28 +0700

    Server lấy data lưu vào bảng orders_item

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import Order from "../../models/order.model";
 import { or } from "sequelize";
 import { generateOrderCode } from "../../helpers/generate";
+import Tour from "../../models/tour.model";
+import OrderItem from "../../models/order-item.model";
 
 //[POST] /order
 export const order = async (req: Request, res: Response) => {
@@ -31,6 +33,30 @@ export const order = async (req: Request, res: Response) => {
       },
     }
   );
+
+  // Lưu data vào bảng orders_item
+  for (const item of data.cart) {
+    const dataItem = {
+      orderId: orderId,
+      tourId: item.tourId,
+      quantity: item.quantity,
+    };
+
+    const infoTour = await Tour.findOne({
+      where: {
+        id: item.tourId,
+        deleted: false,
+        status: "active",
+      },
+      raw: true,
+    });
+
+    (dataItem as any).price = (infoTour as any).price;
+    (dataItem as any).discount = (infoTour as any).discount;
+    (dataItem as any).timeStart = (infoTour as any).timeStart;
+
+    await OrderItem.create(dataItem);
+  }
 
   res.json({
     code: 200,
